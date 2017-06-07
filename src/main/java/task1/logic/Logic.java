586e042ec7844a465e7b706eef15a9828f464e79
pavelkor91 <pavelkor91@gmail.com>
2017-06-07 14:06:27 +0300

    Задание 1 - Запуск из командной строки (создан bat файл с названием run.bat)

---
+++ src/main/java/task1/logic/Logic.java
@@ -0,0 +1,8 @@
+package main.java.task1.logic;
+
+
+public class Logic {
+    public String method(){
+        return "I am string in Logic.";
+    }
+}
---
+++ src/main/java/task1/run.bat
@@ -0,0 +1,8 @@
+cd ../../../..
+
+mkdir bin
+
+javac -sourcepath ./src -d bin src/main/java/task1/main/Main.java
+java -classpath ./bin main.java.task1.main.Main
+
+pause
