6316e023798ddea6e9a8dffbe740017cff16537f
pavelkor91 <pavelkor91@gmail.com>
2017-06-09 11:49:23 +0300

    Добавлен maven модуль в проект

---
+++ src/main/java/task6/Note.java
@@ -0,0 +1,14 @@
+package task6;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Note {
+
+    private String title;
+    private String message;
+
+}
